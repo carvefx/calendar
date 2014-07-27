d734ecf99b763f6a93b9858fc4efcf99a9c885f9
Adrian Mihai <adrian@carve.ro>
2014-07-27 15:12:58 +0300

    + refactored methods related to wrapping the Carbon library in their own class

---
+++ src/Carvefx/Calendar/AbstractCarbonWrapper.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace Carvefx\Calendar;
+
+
+class AbstractCarbonWrapper
+{
+  /**
+   * @var \Carbon\Carbon
+   */
+  protected $carbon;
+
+  /**
+   * Attempts to query Carbon for the property
+   *
+   * @param $prop
+   * @return \DateTimeZone|int|string
+   */
+  public function __get($prop)
+  {
+    return $this->carbon->$prop;
+  }
+
+  /**
+   * Attempts to query Carbon for the property
+   *
+   * @param $method
+   * @param $args
+   * @return mixed
+   */
+  public function __call($method, $args)
+  {
+    $result = $this->carbon->$method($args);
+    if (!$this->isModifierMethod($method)) {
+      return $result;
+    }
+
+    return $this;
+  }
+
+  /**
+   * @param $method
+   * @return bool
+   */
+  protected function isModifierMethod($method)
+  {
+    $pattern = '/^add|^sub/';
+    preg_match($pattern, $method, $matches);
+    if (count($matches)) {
+      return true;
+    }
+
+    return false;
+  }
+}
--- src/Carvefx/Calendar/Day.php
+++ src/Carvefx/Calendar/Day.php
@@ -4,13 +4,8 @@ namespace Carvefx\Calendar;
 
 use Carbon\Carbon;
 
-class Day
+class Day extends AbstractCarbonWrapper
 {
-  /**
-   * @var \Carbon\Carbon
-   */
-  private $carbon;
-
   /**
    * @var bool
    */
@@ -50,46 +45,5 @@ class Day
   public function isBlankDay()
   {
     return $this->blank_day;
-  }
-
-  /**
-   * Attempts to query Carbon for the property
-   * @param $prop
-   * @return \DateTimeZone|int|string
-   */
-  public function __get($prop)
-  {
-    return $this->carbon->$prop;
-  }
-
-  /**
-   * Attempts to query Carbon for the property
-   * @param $method
-   * @param $args
-   * @return mixed
-   */
-  public function __call($method, $args)
-  {
-    $result = $this->carbon->$method($args);
-    if(! $this->isModifierMethod($method)) {
-      return $result;
-    }
-
-    return $this;
-  }
-
-  /**
-   * @param $method
-   * @return bool
-   */
-  private function isModifierMethod($method)
-  {
-    $pattern = '/^add|^sub/';
-    preg_match($pattern, $method, $matches);
-    if (count($matches)) {
-      return true;
-    }
-
-    return false;
   }
 }

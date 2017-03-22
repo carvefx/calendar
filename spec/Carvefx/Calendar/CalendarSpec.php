46a06db7b345c8bfd137230efb7d54541da4c14d
Micheal Mand <micheal@kmdwebdesigns.com>
2017-03-22 16:55:38 -0600

    Update PHPSpec version

--- spec/Carvefx/Calendar/CalendarSpec.php
+++ spec/Carvefx/Calendar/CalendarSpec.php
@@ -10,18 +10,18 @@ use PhpSpec\ObjectBehavior;
 
 class CalendarSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(Calendar::class);
     }
 
 
-    function let()
+    public function let()
     {
         $this->beConstructedWith(2014, 7);
     }
 
-    function it_returns_the_weeks_for_a_month()
+    public function it_returns_the_weeks_for_a_month()
     {
         $weeks = $this->getWeeks();
         $weeks->shouldBeArray();
@@ -40,14 +40,14 @@ class CalendarSpec extends ObjectBehavior
         $days[6]->toDateString()->shouldReturn('2014-08-02');
     }
 
-    function it_returns_the_date_for_the_first_day()
+    public function it_returns_the_date_for_the_first_day()
     {
         $date = Carbon::create(2014, 7, 1, 0);
         $returned_date = $this->getFirstDay();
         $returned_date->toDateTimeString()->shouldBe($date->toDateTimeString());
     }
 
-    function it_returns_the_date_for_the_last_day()
+    public function it_returns_the_date_for_the_last_day()
     {
         $date = Carbon::create(2014, 7, 31, 0);
         $returned_date = $this->getLastDay();
--- spec/Carvefx/Calendar/DaySpec.php
+++ spec/Carvefx/Calendar/DaySpec.php
@@ -8,53 +8,53 @@ use PhpSpec\ObjectBehavior;
 
 class DaySpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(Day::class);
     }
 
-    function let()
+    public function let()
     {
         $this->beConstructedWith(2014, 7, 27);
     }
 
-    function it_forwards_property_gets_to_carbon()
+    public function it_forwards_property_gets_to_carbon()
     {
         $this->day->shouldBe(27);
     }
 
-    function it_forwards_method_calls_to_carbon()
+    public function it_forwards_method_calls_to_carbon()
     {
         $expected = Carbon::create(2014, 7, 27, 0)->toDateString();
         $this->toDateString()->shouldBe($expected);
     }
 
-    function it_defaults_to_a_normal_day()
+    public function it_defaults_to_a_normal_day()
     {
         $this->isBlankDay()->shouldReturn(false);
     }
 
-    function it_can_be_set_as_a_blank_day()
+    public function it_can_be_set_as_a_blank_day()
     {
         $this->setBlankDay(true);
         $this->isBlankDay()->shouldReturn(true);
     }
 
-    function it_throws_an_exception_when_attempting_to_set_blank_day_status_as_non_boolean()
+    public function it_throws_an_exception_when_attempting_to_set_blank_day_status_as_non_boolean()
     {
         $this->shouldThrow('\InvalidArgumentException')->duringSetBlankDay('party like it\'s 1999');
         $this->shouldThrow('\InvalidArgumentException')->duringSetBlankDay(1999);
         $this->shouldThrow('\InvalidArgumentException')->duringSetBlankDay('1');
     }
 
-    function it_returns_true_if_today()
+    public function it_returns_true_if_today()
     {
         $today = Carbon::now();
         $this->beConstructedWith($today->year, $today->month, $today->day);
         $this->isToday()->shouldReturn(true);
     }
 
-    function it_does_not_return_the_carbon_object_when_applying_modifiers()
+    public function it_does_not_return_the_carbon_object_when_applying_modifiers()
     {
         $this->addDays(3)->shouldReturnAnInstanceOf(Day::class);
         $this->toDateString()->shouldBe('2014-07-30');
